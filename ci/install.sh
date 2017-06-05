6832a4af844839e97f5214bb82eb83a1d3b0b1a1
Jorge Aparicio <jorge@japaric.io>
2017-06-05 10:36:52 -0500

    fix rustfmt install

--- ci/install.sh
+++ ci/install.sh
@@ -22,6 +22,7 @@ main() {
     if [ ! -z $VENDOR ]; then
         curl -LSfs https://japaric.github.io/trust/install.sh | \
             sh -s -- \
+               --crate rustfmt \
                --force \
                --git japaric/rustfmt-bin \
                --tag v0.8.4-20170605
