9ed581c99ca730a1d173b2fcd30f2b2d782e9754
Jorge Aparicio <jorge@japaric.io>
2017-06-06 14:29:03 -0500

    add regression tests for #92 and #99
    closes #92

--- ci/script.sh
+++ ci/script.sh
@@ -138,7 +138,7 @@ main() {
             # test_svd MK70F15WS
 
             # OK
-            # NOTE it would take too long to test all these so we only test half
+            # NOTE it would take too long to test all these so we only test one third
             test_svd MK02F12810
             # test_svd MK10D10
             # test_svd MK10D5
@@ -230,42 +230,42 @@ main() {
             # test_svd MKW40Z4
             # test_svd MKW41Z4
 
-            # FIXME(#92) "duplicate definitions with name `bits`"
-            # test_svd MKE02Z2
-            # test_svd MKE02Z4
+            # #92 regression tests
+            # NOTE it would take too long to test all these so we only test one third
+            test_svd MKE02Z4
             # test_svd MKE04Z1284
             # test_svd MKE04Z4
-            # test_svd MKE06Z4
+            test_svd MKE06Z4
             # test_svd MKE14D7
             # test_svd MKE15D7
-            # test_svd MKL02Z4
+            test_svd MKL02Z4
             # test_svd MKL03Z4
             # test_svd MKL04Z4
-            # test_svd MKL05Z4
+            test_svd MKL05Z4
             # test_svd MKL13Z644
             # test_svd MKL14Z4
-            # test_svd MKL15Z4
+            test_svd MKL15Z4
             # test_svd MKL16Z4
             # test_svd MKL17Z4
-            # test_svd MKL17Z644
+            test_svd MKL17Z644
             # test_svd MKL24Z4
             # test_svd MKL25Z4
-            # test_svd MKL26Z4
+            test_svd MKL26Z4
             # test_svd MKL27Z4
             # test_svd MKL27Z644
-            # test_svd MKL33Z4
+            test_svd MKL33Z4
             # test_svd MKL33Z644
             # test_svd MKL34Z4
-            # test_svd MKL36Z4
+            test_svd MKL36Z4
             # test_svd MKL43Z4
             # test_svd MKL46Z4
-            # test_svd MKM14ZA5
+            test_svd MKM14ZA5
             # test_svd MKM33ZA5
             # test_svd MKM34Z7
-            # test_svd MKM34ZA5
+            test_svd MKM34ZA5
             # test_svd MKW01Z4
             # test_svd SKEAZ1284
-            # test_svd SKEAZN642
+            test_svd SKEAZN642
             # test_svd SKEAZN84
         ;;
 
@@ -425,116 +425,120 @@ main() {
             # BAD-SVD uses the identifier '_' to name a reserved bitfield value
             # test_svd LPC1102_4_v4
 
-            # FIXME(#99) "duplicate definitions with name `is_set`"
+            # FIXME(???) "duplicate definitions for `write`"
+            # #99 regression test
             # test_svd LPC5410x_v0.4
         ;;
 
         SiliconLabs)
-            # FIXME(#99) "duplicate definitions with name `is_set`"
-            # test_svd SIM3C1x4_SVD
-            # test_svd SIM3C1x6_SVD
-            # test_svd SIM3C1x7_SVD
-            # test_svd SIM3L1x4_SVD
-            # test_svd SIM3L1x6_SVD
-            # test_svd SIM3L1x7_SVD
+            # #99 regression tests
+            test_svd SIM3C1x4_SVD
+            test_svd SIM3C1x6_SVD
+            test_svd SIM3C1x7_SVD
+            test_svd SIM3L1x4_SVD
+            test_svd SIM3L1x6_SVD
+            test_svd SIM3L1x7_SVD
+            test_svd SIM3U1x4_SVD
+            test_svd SIM3U1x6_SVD
+            test_svd SIM3U1x7_SVD
+
+            # FIXME(???) panicked at "c.text.clone()"
             # test_svd SIM3L1x8_SVD
-            # test_svd SIM3U1x4_SVD
-            # test_svd SIM3U1x6_SVD
-            # test_svd SIM3U1x7_SVD
         ;;
 
         Spansion)
             # OK
-            # test_svd MB9AF12xK
-            # test_svd MB9AF12xL
-            # test_svd MB9AF42xK
-            # test_svd MB9AF42xL
-            # test_svd MB9BF12xJ
-            # test_svd MB9BF12xS
-            # test_svd MB9BF12xT
-            # test_svd MB9BF16xx
-            # test_svd MB9BF32xS
-            # test_svd MB9BF32xT
-            # test_svd MB9BF36xx
-            # test_svd MB9BF42xS
-            # test_svd MB9BF42xT
-            # test_svd MB9BF46xx
-            # test_svd MB9BF52xS
-            # test_svd MB9BF52xT
-            # test_svd MB9BF56xx
+            test_svd MB9AF12xK
+            test_svd MB9AF12xL
+            test_svd MB9AF42xK
+            test_svd MB9AF42xL
+            test_svd MB9BF12xJ
+            test_svd MB9BF12xS
+            test_svd MB9BF12xT
+            test_svd MB9BF16xx
+            test_svd MB9BF32xS
+            test_svd MB9BF32xT
+            test_svd MB9BF36xx
+            test_svd MB9BF42xS
+            test_svd MB9BF42xT
+            test_svd MB9BF46xx
+            test_svd MB9BF52xS
+            test_svd MB9BF52xT
+            test_svd MB9BF56xx
 
             # #102 regression tests
-            test_svd MB9AF10xN
+            # # NOTE it would take too long to test all these so we only test half
+            # test_svd MB9AF10xN
             test_svd MB9AF10xR
-            test_svd MB9AF11xK
+            # test_svd MB9AF11xK
             test_svd MB9AF11xL
-            test_svd MB9AF11xM
+            # test_svd MB9AF11xM
             test_svd MB9AF11xN
-            test_svd MB9AF13xK
+            # test_svd MB9AF13xK
             test_svd MB9AF13xL
-            test_svd MB9AF13xM
+            # test_svd MB9AF13xM
             test_svd MB9AF13xN
-            test_svd MB9AF14xL
+            # test_svd MB9AF14xL
             test_svd MB9AF14xM
-            test_svd MB9AF14xN
+            # test_svd MB9AF14xN
             test_svd MB9AF15xM
-            test_svd MB9AF15xN
+            # test_svd MB9AF15xN
             test_svd MB9AF15xR
-            test_svd MB9AF31xK
+            # test_svd MB9AF31xK
             test_svd MB9AF31xL
-            test_svd MB9AF31xM
+            # test_svd MB9AF31xM
             test_svd MB9AF31xN
-            test_svd MB9AF34xL
+            # test_svd MB9AF34xL
             test_svd MB9AF34xM
-            test_svd MB9AF34xN
+            # test_svd MB9AF34xN
             test_svd MB9AFA3xL
-            test_svd MB9AFA3xM
+            # test_svd MB9AFA3xM
             test_svd MB9AFA3xN
-            test_svd MB9AFA4xL
+            # test_svd MB9AFA4xL
             test_svd MB9AFA4xM
-            test_svd MB9AFA4xN
+            # test_svd MB9AFA4xN
             test_svd MB9AFB4xL
-            test_svd MB9AFB4xM
+            # test_svd MB9AFB4xM
             test_svd MB9AFB4xN
-            test_svd MB9BF10xN
+            # test_svd MB9BF10xN
             test_svd MB9BF10xR
-            test_svd MB9BF11xN
+            # test_svd MB9BF11xN
             test_svd MB9BF11xR
-            test_svd MB9BF11xS
+            # test_svd MB9BF11xS
             test_svd MB9BF11xT
-            test_svd MB9BF12xK
+            # test_svd MB9BF12xK
             test_svd MB9BF12xL
-            test_svd MB9BF12xM
+            # test_svd MB9BF12xM
             test_svd MB9BF21xS
-            test_svd MB9BF21xT
+            # test_svd MB9BF21xT
             test_svd MB9BF30xN
-            test_svd MB9BF30xR
+            # test_svd MB9BF30xR
             test_svd MB9BF31xN
-            test_svd MB9BF31xR
+            # test_svd MB9BF31xR
             test_svd MB9BF31xS
-            test_svd MB9BF31xT
+            # test_svd MB9BF31xT
             test_svd MB9BF32xK
-            test_svd MB9BF32xL
+            # test_svd MB9BF32xL
             test_svd MB9BF32xM
-            test_svd MB9BF40xN
+            # test_svd MB9BF40xN
             test_svd MB9BF40xR
-            test_svd MB9BF41xN
+            # test_svd MB9BF41xN
             test_svd MB9BF41xR
-            test_svd MB9BF41xS
+            # test_svd MB9BF41xS
             test_svd MB9BF41xT
-            test_svd MB9BF50xN
+            # test_svd MB9BF50xN
             test_svd MB9BF50xR
-            test_svd MB9BF51xN
+            # test_svd MB9BF51xN
             test_svd MB9BF51xR
-            test_svd MB9BF51xS
+            # test_svd MB9BF51xS
             test_svd MB9BF51xT
-            test_svd MB9BF52xK
+            # test_svd MB9BF52xK
             test_svd MB9BF52xL
-            test_svd MB9BF52xM
+            # test_svd MB9BF52xM
             test_svd MB9BF61xS
-            test_svd MB9BF61xT
+            # test_svd MB9BF61xT
             test_svd MB9BFD1xS
-            test_svd MB9BFD1xT
+            # test_svd MB9BFD1xT
         ;;
 
         STMicro)
