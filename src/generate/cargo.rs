cfa593da0daa83757dd62f15083294e36ac2322a
James Munns <james.munns@ferrous-systems.com>
2018-10-23 16:42:03 +0200

    Features in default should be single quoted

--- src/generate/cargo.rs
+++ src/generate/cargo.rs
@@ -1,6 +1,7 @@
 pub fn generate_skeleton(features: Vec<String>) -> String {
     format!(
         "[features]
+
 # Auto generated feature flags
 # By default, all peripherals are enabled for use. To speed build times, select
 # `--no-default-features`, and re-enable peripherals necessary for your use
@@ -8,7 +9,7 @@ default = [ {} ]
 
 # Individual Peripherals
 {}",
-        features.join(", "),
+        features.iter().map(|feat| format!("'{}'", feat)).collect::<Vec<_>>().join(", "),
         features.iter().fold(String::new(), |mut s, f| {
             s.push_str(&format!("{} = []\n", f));
             s
