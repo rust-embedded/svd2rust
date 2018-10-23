5ccc2af648ea8f752a292749f9bba01286c215e3
James Munns <james.munns@ferrous-systems.com>
2018-10-23 16:31:37 +0200

    Restore `[features]` section, add some comments

--- src/generate/cargo.rs
+++ src/generate/cargo.rs
@@ -1,8 +1,12 @@
 pub fn generate_skeleton(features: Vec<String>) -> String {
     format!(
-        "# THIS TOML FILE WAS GENERATED AND IS INCOMPLETE!
+        "[features]
 # Auto generated feature flags
+# By default, all peripherals are enabled for use. To speed build times, select
+# `--no-default-features`, and re-enable peripherals necessary for your use
 default = [ {} ]
+
+# Individual Peripherals
 {}",
         features.join(", "),
         features.iter().fold(String::new(), |mut s, f| {
