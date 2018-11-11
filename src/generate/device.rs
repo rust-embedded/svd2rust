f811a2529112e3fceb558f0f3e4168d64cead126
Jorge Aparicio <jorge@japaric.io>
2018-11-11 17:41:51 +0100

    remove the const_fn feature gate
    it doesn't seem to be needed anymore

--- src/generate/device.rs
+++ src/generate/device.rs
@@ -45,7 +45,6 @@ pub fn render(d: &Device, target: &Target, nightly: bool, device_x: &mut String)
 
     if *target != Target::CortexM {
         out.push(quote! {
-            #![feature(const_fn)]
             #![feature(try_from)]
         });
     }
