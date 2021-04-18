cc39d5e7d7103a2f02cd64e53bff9a081ba7ec9e
YuhanLiin <yuhanliin+github@protonmail.com>
2021-04-17 20:45:47 -0400

    Modify MSP430 atomic code to be compatible with new API

--- src/generate/generic_msp430_atomic.rs
+++ src/generate/generic_msp430_atomic.rs
@@ -1,21 +1,21 @@
-impl<U, REG> Reg<U, REG>
+impl<REG: Writable> Reg<REG>
 where
     Self: Readable + Writable,
-    U: AtomicOperations + Default + Copy + Not<Output = U>,
+    REG::Ux: AtomicOperations + Default + Not<Output = REG::Ux>,
 {
     ///Set high every bit in the register that was set in the write proxy. Leave other bits
     ///untouched. The write is done in a single atomic instruction.
     #[inline(always)]
     pub unsafe fn set_bits<F>(&self, f: F)
     where
-        F: FnOnce(&mut W<U, Self>) -> &mut W<U, Self>,
+        F: FnOnce(&mut W<REG>) -> &mut W<REG>,
     {
         let bits = f(&mut W {
             bits: Default::default(),
             _reg: marker::PhantomData,
         })
         .bits;
-        U::atomic_or(self.register.as_ptr(), bits);
+        REG::Ux::atomic_or(self.register.as_ptr(), bits);
     }
 
     ///Clear every bit in the register that was cleared in the write proxy. Leave other bits
@@ -23,14 +23,14 @@ where
     #[inline(always)]
     pub unsafe fn clear_bits<F>(&self, f: F)
     where
-        F: FnOnce(&mut W<U, Self>) -> &mut W<U, Self>,
+        F: FnOnce(&mut W<REG>) -> &mut W<REG>,
     {
         let bits = f(&mut W {
-            bits: !U::default(),
+            bits: !REG::Ux::default(),
             _reg: marker::PhantomData,
         })
         .bits;
-        U::atomic_and(self.register.as_ptr(), bits);
+        REG::Ux::atomic_and(self.register.as_ptr(), bits);
     }
 
     ///Toggle every bit in the register that was set in the write proxy. Leave other bits
@@ -38,13 +38,13 @@ where
     #[inline(always)]
     pub unsafe fn toggle_bits<F>(&self, f: F)
     where
-        F: FnOnce(&mut W<U, Self>) -> &mut W<U, Self>,
+        F: FnOnce(&mut W<REG>) -> &mut W<REG>,
     {
         let bits = f(&mut W {
             bits: Default::default(),
             _reg: marker::PhantomData,
         })
         .bits;
-        U::atomic_xor(self.register.as_ptr(), bits);
+        REG::Ux::atomic_xor(self.register.as_ptr(), bits);
     }
 }
